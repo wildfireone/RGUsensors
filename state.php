10f4bdab2a4473548337a4a62502fb887bb60445
CMackintosh <calum.mackintosh@btinternet.com>
2015-07-30 11:16:47 +0100

    json array

--- state.php
+++ state.php
@@ -4,6 +4,6 @@ ini_set('error_reporting', E_ALL);
 error_reporting(-1);
 
 $json = file_get_contents('https://data.sparkfun.com/output/3J5xqbLGGqcJQqMor7g8.json');
-$obj = json_decode($json);
+$obj = json_decode($json,true);
 echo($obj[0]["humidity"]);
 ?>
