daa78a2011c4a5de93f4ea6cc85e9f20c4b0fb22
CMackintosh <calum.mackintosh@btinternet.com>
2015-07-30 10:50:30 +0100

    state2

--- state.php
+++ state.php
@@ -1,16 +1,16 @@
 <?php
 error_reporting(E_ALL);
 $running = true;
-echo running;
+print $running;
 if($_GET["switch"]== "off"){
-	running = false;
+	$running = false;
 }
 else if($_GET["switch"]== "on"){
-	running = true;
+	$running = true;
 }
 
 if($_GET["check"] == "check")
 {
-	echo running;
+	echo $running;
 }
 ?>
