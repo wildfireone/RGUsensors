526bb039696af58174c9362a96601d4e1dae1e03
CMackintosh <calum.mackintosh@btinternet.com>
2015-07-30 11:05:45 +0100

    errors

--- state.php
+++ state.php
@@ -1,5 +1,8 @@
 <?php
 error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+error_reporting(-1);
+
 $json = file_get_contents('https://data.sparkfun.com/output/3J5xqbLGGqcJQqMor7g8.json');
 $obj = json_decode($json);
 echo $obj->access_token;
