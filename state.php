c8b9591700e4972a897635a4a0712a3d42d42d95
CMackintosh <calum.mackintosh@btinternet.com>
2015-07-30 11:45:27 +0100

    line

--- state.php
+++ state.php
@@ -5,6 +5,6 @@ error_reporting(-1);
 
 $json = file_get_contents('https://data.sparkfun.com/output/3J5xqbLGGqcJQqMor7g8.json');
 $obj = json_decode($json,true);
-echo($obj[0]["humidity"]);
+echo($obj[0]["humidity"]<br/>);
 
 ?>
