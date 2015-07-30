a2b84b11994b4b0bcea3aee64f7c672f80ff08ba
CMackintosh <calum.mackintosh@btinternet.com>
2015-07-30 11:03:29 +0100

    json2

--- state.php
+++ state.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-$data = file_get_contents(https://data.sparkfun.com/streams/3J5xqbLGGqcJQqMor7g8.json); 
-$json = json_decode($data);
-var_dump($json);
+$json = file_get_contents('https://data.sparkfun.com/streams/3J5xqbLGGqcJQqMor7g8.json');
+$obj = json_decode($json);
+echo $obj->access_token;
 ?>
