1424632c0a1a1b349bff5bf3fe07dcac015acbc5
CMackintosh <calum.mackintosh@btinternet.com>
2015-07-30 11:07:38 +0100

    errror2

--- state.php
+++ state.php
@@ -5,5 +5,5 @@ error_reporting(-1);
 
 $json = file_get_contents('https://data.sparkfun.com/output/3J5xqbLGGqcJQqMor7g8.json');
 $obj = json_decode($json);
-echo $obj->access_token;
+print($obj);
 ?>
